ee0af6404b1e0142f4274982f4eade50f8553d24
hackpython <prakashvidya368@gmail.com>
2025-01-23 15:56:19 +0530

    Completed the calculator app

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,7 +4,24 @@ let digitalstr = '';
 setInterval(() => {
     let screen = document.getElementById('main-screen')
     screen.value = digitalstr;
-    console.log(screen);
-    
+}, 10);
 
-}, 1000);
+function editdigitalstr(a){
+    if (a=='d') {
+        digitalstr = digitalstr.slice(0,(digitalstr.length-1))
+    }else{
+        digitalstr += a;
+    }
+}
+
+document.getElementById('reset').addEventListener('click',()=>{
+    digitalstr = ''
+})
+
+document.getElementById('calcu').addEventListener('click',()=>{
+    try {
+        digitalstr = eval(digitalstr);
+    } catch (error) {
+        digitalstr = "Error";
+    }
+})
