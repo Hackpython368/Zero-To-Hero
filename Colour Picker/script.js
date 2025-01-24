1a5c847e46534e0370e409801c8e00acf859dff5
hackpython <prakashvidya368@gmail.com>
2025-01-24 21:34:31 +0530

    Completed the third program that is color picker .

---
+++ Colour Picker/script.js
@@ -0,0 +1,3 @@
+document.getElementById('set').addEventListener('click',()=>{
+    let body = document.getElementById('container').style.backgroundColor = document.getElementById('color').value;
+})
